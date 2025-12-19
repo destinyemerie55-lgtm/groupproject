ad6c74136d985503845b447a2cb7794cbcc97971
dxtny <destinyemerie6@gmail.com>
2025-12-19 11:38:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// document object model 
+
+// dom manipulation
+
+// dom manipulation
+//  we acces elements with id class eg
+
+let Zam = document.getElementById("test2"); 
+Zam.textContent = "Welcome Zam";
+Zam.style.backgroundColor = "red";
+Zam.style.color = "blue";
+Zam.style.fontSize = "70px";
+console.log(Zam);
+
+
+// let element = document.getElementById("test2");
+// element.textcontent = "Hello Unmei";
+// element.style.color = "blue";
+// ele
+// console.log(element);
